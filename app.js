ace8465fa74af267d1c6604677b50498735e245d
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-11-04 10:19:59 -0700

    Partial solution completed

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.table(animals)
 
     //select header by id
-
+    const $header = document.getElementById('header')
 
     //console.log(header) what do you think you'll see?
-    // console.log(header)
+    console.log($header)
 
     //change color of header text
+    $header.style = "color: red"
 
     //select card container div by class
-
+    const $cardContainer = document.querySelector('.card-container')
 
 
     //console.log cardContainer to ensure it has been selected accurately
@@ -27,57 +28,60 @@ document.addEventListener('DOMContentLoaded', () => {
         const animalImage = animal.image
 
         //create <div> element for card
-
+        const $card = document.createElement('div')
 
 
         //add "card" class to card div
         //write code on whiteboard
+        $card.className = 'card'
 
 
         //append "card" div to cardContainer
-
+        $cardContainer.appendChild($card)
 
         //create image element
-
+        const $image = document.createElement('img')
 
         //console.log image
 
         //add class name attribute to image element
+        $image.className = 'image'
 
         //console.log image
 
         //add src to image element
         //write code on whiteboard
-
+        $image.src = animalImage
 
         //append image to parent (card div)
-
+        $card.appendChild($image)
         //Check to ensure images rendered w console.log
 
-        //create div with class "card-body"
-
+        //create card-body div 
+        const $cardBody = document.createElement('div')
 
         //give card-body div a class of "card-body"
-
+        $cardBody.className = 'card-body'
 
         //append card-body div to card div
-
+        $card.appendChild($cardBody)
 
         //create h5 tag for card title
+        const cardTitle = document.createElement('h5')
 
 
         //add "card-title" class to card title
-
+        cardTitle.className = 'card-title'
 
         //add title of animal's name
-
+        cardTitle.innerText = animalName
 
         //append h5 tag to card body div
-
+        $cardBody.appendChild(cardTitle)
 
 
         //can stop here unless you would like to keep going
-
+        
         //create p tag 
 
         //add "card-text" class to p tag
